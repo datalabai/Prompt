2f90c8904b271d3bf73639217d7c83765b0a7d25
shiva <shiva@shivas-MacBook-Pro.local>
2024-06-06 03:25:49 +0530

    modified logo and welcome text

--- app/page.js
+++ app/page.js
@@ -5,7 +5,13 @@ const Home = () => {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
             <div className="flex flex-col  w-fit p-2 absolute top-[45%]">
-                <h1 className="text-3xl font-bold mb-4 ml-12">Welcome to Datalab AI</h1>
+                <h1 className="text-3xl font-bold mb-4 ml-12">Welcome to   <font color="#FF2626">P</font>
+            <font color="#252A34">R</font>
+            <font color="#753422">O</font>
+            <font color="#FFD523">M</font>
+            <font color="#71EFA3">P</font>
+            <font color="#FFCCCC">T</font>
+            </h1>
                 <p className="text-lg text-gray-600 mb-8">Start Engaging in Channels to interact with Models</p>
             </div>
         </div>
