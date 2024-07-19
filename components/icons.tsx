64954f3614a28201a893ca6c7fa1e5a5a74b4645
shiva <shiva@shivas-MacBook-Pro.local>
2024-07-19 13:14:52 +0530

    added prompt logo

--- components/icons.tsx
+++ components/icons.tsx
@@ -2,32 +2,7 @@ type IconProps = React.HTMLAttributes<SVGElement>
 
 export const Icons = {
   logo: (props: IconProps) => (
-    // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" {...props}>
-    //   <rect width="256" height="256" fill="none" />
-    //   <line
-    //     x1="208"
-    //     y1="128"
-    //     x2="128"
-    //     y2="208"
-    //     fill="none"
-    //     stroke="currentColor"
-    //     strokeLinecap="round"
-    //     strokeLinejoin="round"
-    //     strokeWidth="16"
-    //   />
-    //   <line
-    //     x1="192"
-    //     y1="40"
-    //     x2="40"
-    //     y2="192"
-    //     fill="none"
-    //     stroke="currentColor"
-    //     strokeLinecap="round"
-    //     strokeLinejoin="round"
-    //     strokeWidth="16"
-    //   />
-    // </svg>
-    <image path="../favicon.ico" {...props} />
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-terminal ml-6"><polyline points="4 17 10 11 4 5"/><line x1="12" x2="20" y1="19" y2="19"/></svg>
   ),
   twitter: (props: IconProps) => (
     <svg
