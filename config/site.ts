5bc13eca6881158b66d78b1df0609b4edbc10c82
shiva <shiva@shivas-MacBook-Pro.local>
2024-07-22 18:37:33 +0530

    updatd links and revmoved built by

--- config/site.ts
+++ config/site.ts
@@ -1,12 +1,12 @@
 export const siteConfig = {
   name: "PROMPT",
-  url: "https://prompt.com",
-  ogImage: "https://ui.shadcn.com/og.jpg",
+  url: "https://promptexpert.xyz/",
+  ogImage: "./mobileview.png",
   description:
     "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn-ui/ui",
+    twitter: "https://promptexpert.xyz/",
+    github: "https://promptexpert.xyz/",
   },
 }
 
