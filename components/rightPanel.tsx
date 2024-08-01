9fe7deed155d3a3373f9f9a8a2f7de340f63ff4e
shiva <shiva@shivas-MacBook-Pro.local>
2024-08-01 10:10:34 +0530

    added maximize and minimize for right pannel

--- components/rightPanel.tsx
+++ components/rightPanel.tsx
@@ -1,14 +1,30 @@
-import { RightNotifications } from "./rightpanel-notification";
-
+import React, { useState } from 'react';
+import { RightNotifications } from './rightpanel-notification'; 
+import { Maximize2, Minimize2 } from 'lucide-react';
 
 
 export default function RightPanel() {
+  const [isMaximized, setIsMaximized] = useState(false);
+
+  const togglePanel = () => {
+    setIsMaximized(prevState => !prevState);
+  };
+
   return (
-    <div className="relative m-2 px-3 ">
-        <div className="max-w-xl shadow">
-          <RightNotifications />
-        </div>
-      
+    <div className={`relative m-2 px-3 transition-all ${isMaximized ? 'w-full' : 'w-1/2'} max-w-xl`}>
+      <div className={`transition-all ${isMaximized ? 'h-screen' : ''} overflow-auto`}>
+        <RightNotifications />
+      </div>
+      <button
+        onClick={togglePanel}
+        className="absolute top-2 right-3 p-2 "
+      >
+        {isMaximized ? (
+          <Minimize2  /> 
+        ) : (
+          <Maximize2  /> 
+        )}
+      </button>
     </div>
-  )
+  );
 }
