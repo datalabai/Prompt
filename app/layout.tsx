9ca3e33e3da71c49ae2fb31a4c84dd2b9d9c2efa
shivapabbu <shiva.pabbu@gmail.com>
2024-08-03 01:47:54 +0530

    added metadata changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 "use client";
 import "@/config/globals.css";
-import { Metadata, Viewport } from "next";
+import { Viewport } from "next";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
-import { siteConfig } from "@/config/site";
+
 import { fontSans } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers";
@@ -17,57 +17,7 @@ import GoogleAnalytics from "./GoogleAnalytics";
 import RightPanel from "@/components/rightPanel";
 import { useState } from "react";
 
-// export const metadata: Metadata = {
-//   title: {
-//     default: siteConfig.name,
-//     template: `%s - ${siteConfig.name}`,
-//   },
-//   metadataBase: new URL(siteConfig.url),
-//   description: siteConfig.description,
-//   keywords: [
-//     "Next.js",
-//     "React",
-//     "Tailwind CSS",
-//     "Server Components",
-//     "Radix UI",
-//   ],
-//   authors: [
-//     {
-//       name: "prompt",
-//       url: "https://promptexpert.xyz/",
-//     },
-//   ],
-//   creator: "prompt",
-//   openGraph: {
-//     type: "website",
-//     locale: "en_US",
-//     url: siteConfig.url,
-//     title: siteConfig.name,
-//     description: siteConfig.description,
-//     siteName: siteConfig.name,
-//     images: [
-//       {
-//         url: siteConfig.ogImage,
-//         width: 1200,
-//         height: 630,
-//         alt: siteConfig.name,
-//       },
-//     ],
-//   },
-//   twitter: {
-//     card: "summary_large_image",
-//     title: siteConfig.name,
-//     description: siteConfig.description,
-//     images: [siteConfig.ogImage],
-//     creator: "@prompt",
-//   },
-//   icons: {
-//     icon: "/favicon.ico",
-//     shortcut: "/favicon-16x16.png",
-//     apple: "/apple-touch-icon.png",
-//   },
-//   manifest: `${siteConfig.url}`,
-// };
+
 
 export const viewport: Viewport = {
   themeColor: [
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,68 @@ import Image from "next/image"
 
 
 import MailPage from "./mail/page"
-import { auth } from "./firebase"
-import { use } from "react"
+import { siteConfig } from "@/config/site";
+import { Metadata, Viewport } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: [
+    "prompt",
+    "expert",
+    "image",
+    "resume",
+    "text",
+    "logo",
+    "design",
+    "writing",
+    "writing prompt",
+    "memes",
+    "meme generator",
+    "generator",
+    "text generator",
+  ],
+  authors: [
+    {
+      name: "prompt",
+      url: "https://prompt.fun/",
+    },
+  ],
+  creator: "prompt",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@prompt",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}`,
+};
 
 export default function IndexPage() {
   return (
