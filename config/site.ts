b9e7ed1caa945f249448a5ba7c15cc6b99459528
Bhautik Bavadiya <hi@yesbhautik.co.in>
2024-08-03 15:17:18 +0530

    Improve SEO: Add Keywords and Site Description
    - Added SEO-friendly keywords to enhance search engine visibility.
    - Included a comprehensive site description for better search engine indexing.

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
   url: "https://prompt.fun/",
   ogImage: "./mobileview.png",
   description:
-    "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+    "Discover our AI-powered platform offering a suite of creative tools including content creation, chatbot assistance, writing prompts, image generation, logo design, meme making, and resume building. Enhance your digital projects with hyper-realistic art, custom designs, and user-friendly editing tools. Perfect for artists, designers, and professionals seeking innovative solutions for their creative needs.",
   links: {
     twitter: "https://prompt.fun/",
     github: "https://prompt.fun/",
