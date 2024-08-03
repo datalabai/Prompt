dfc6b3993b0897832bd5b6df1a7c3579e7a53d97
Bhautik Bavadiya <hi@yesbhautik.co.in>
2024-08-03 15:43:08 +0530

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "PROMPT",
   url: "https://prompt.fun/",
-  ogImage: "./promp.fun_preview.png",
+  ogImage: "https://pub-f858c7e521464395920779af297fb1bd.r2.dev/promp.fun_preview.png",
   description:
     "Discover our AI-powered platform offering a suite of creative tools including content creation, chatbot assistance, writing prompts, image generation, logo design, meme making, and resume building. Enhance your digital projects with hyper-realistic art, custom designs, and user-friendly editing tools. Perfect for artists, designers, and professionals seeking innovative solutions for their creative needs.",
   links: {
