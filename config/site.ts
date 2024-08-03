39e36ad9e8013934076a7cd4dcf52f0951f61d4b
Bhautik Bavadiya <hi@yesbhautik.co.in>
2024-08-03 15:19:43 +0530

    Update Preview Image: Rounded Corners and Reduced Size
    - Added a new preview image with rounded corners for a modern look.
    - Reduced the preview image size to be 10x smaller for faster loading.
    - Changed the preview image for the website URL.

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "PROMPT",
   url: "https://prompt.fun/",
-  ogImage: "./mobileview.png",
+  ogImage: "./promp.fun_preview.png",
   description:
     "Discover our AI-powered platform offering a suite of creative tools including content creation, chatbot assistance, writing prompts, image generation, logo design, meme making, and resume building. Enhance your digital projects with hyper-realistic art, custom designs, and user-friendly editing tools. Perfect for artists, designers, and professionals seeking innovative solutions for their creative needs.",
   links: {
