63dacf808add2dc0af7d414f85210369864e707f
shivapabbu <shiva.pabbu@gmail.com>
2024-08-08 18:08:47 +0530

    changes for landing page

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -15,7 +15,7 @@ export function MainNav() {
     <div className="mr-4 hidden md:flex">
       <Link href="/" className="mr-6 flex items-center space-x-2">
         <Icons.logo className="h-6 w-6" />
-        <span className="hidden font-bold sm:inline-block">
+        <span className="hidden font-bold sm:inline-block inline bg-gradient-to-r from-[#16aad3]  to-[#07bc0c] text-transparent bg-clip-text">
           {siteConfig.name}
         </span>
       </Link>
