356007ab25a5d130239d4097cd2e4dd7b546c51c
Kishore <kishore@Straws-MacBook-Pro.local>
2024-08-16 16:55:35 +0530

    added Google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 "use client";
+
 import { ReactNode, use } from "react";
 import "@/config/globals.css";
 import { Viewport } from "next";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-
-
 import { fontSans } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers";
@@ -14,14 +13,13 @@ import { ThemeSwitcher } from "@/components/theme-switcher";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
 import { AuthContextProvider } from "./context/AuthContext";
-import GoogleAnalytics from "./GoogleAnalytics";
 import HotjarSnippet from "./Hotjar";
 import RightPanel from "@/components/rightPanel";
 import { useState } from "react";
 import { usePathname } from 'next/navigation';
 import { CategoryProvider } from './context/CategoryContext';
-
-
+import { GoogleTagManager } from '@next/third-parties/google';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const viewport: Viewport = {
   themeColor: [
@@ -39,44 +37,44 @@ export default function RootLayout({ children }: RootLayoutProps) {
   const [showRightPanel, setShowRightPanel] = useState(true);
   const currentPath = usePathname();
 
-
   const toggleRightPanel = () => {
     setShowRightPanel(prev => !prev);
   };
+
   return (
     <>
       <html lang="en" suppressHydrationWarning>
         <head />
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID || ""} />
         <body
           className={cn(
-            " bg-background font-sans antialiased overflow-hidden",
+            "bg-background font-sans antialiased overflow-hidden",
             fontSans.variable
           )}
         >
-          <GoogleAnalytics />
           <AuthContextProvider>
             <CategoryProvider>
-            <ToastContainer />
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-            >
-               <SiteHeader toggleRightPanel={toggleRightPanel} />
-             <div vaul-drawer-wrapper="" className="flex justify-between mt-2 h-dvh max-w-screen">
-             
-                <div className="relative flex min-h-screen flex-col bg-background mt-2 w-full">
-                  {children}
+              <ToastContainer />
+              <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+              >
+                <SiteHeader toggleRightPanel={toggleRightPanel} />
+                <div vaul-drawer-wrapper="" className="flex justify-between mt-2 h-dvh max-w-screen">
+                  <div className="relative flex min-h-screen flex-col bg-background mt-2 w-full">
+                    {children}
+                  </div>
                 </div>
-              </div>
-              <TailwindIndicator />
-              <ThemeSwitcher />
-            </ThemeProvider>
-            <HotjarSnippet/>
+                <TailwindIndicator />
+                <ThemeSwitcher />
+              </ThemeProvider>
+              <HotjarSnippet />
             </CategoryProvider>
           </AuthContextProvider>
         </body>
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || ""} />
       </html>
     </>
   );
