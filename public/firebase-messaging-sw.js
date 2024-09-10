e68db1ebe5cb3879f300e296b2d30f9b164bb206
Kishore <kishore@Straws-MacBook-Pro.local>
2024-09-10 16:59:50 +0530

    Added Notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,16 +4,16 @@ importScripts(
 );
 
 // Replace these with your own Firebase config keys...
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+fetch('/api/firebase-config')
+  .then(response => response.json())
+  .then(firebaseConfig => {
+    console.log(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
+  })
+  .catch(error => {
+    console.error('Error fetching Firebase config:', error);
+  });
 
-firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
